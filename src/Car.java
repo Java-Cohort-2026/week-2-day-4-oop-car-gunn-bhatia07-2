f25dbdb07f297b78b95499b2ec025b1e7cb0cd81
Gunn Bhatia <gunnbhatia07@gmail.com>
2026-05-12 09:26:06 +0530

    Week 2 Day 4!

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,22 @@
 public class Car {
     // TODO: Define the 3 attributes requested in Main.java
     // (make, model, year)
-    
+    String make;
+    String model;
+    int year;
+
     // TODO: Define an integer attribute for 'speed', set to 0 initially
+    int speed = 0;
     
     // TODO: Write an 'accelerate' method that takes an integer amount and adds it to speed
-    
+    void accelerate (int amount) {
+        speed = speed + amount;
+        System.out.println("Accelerated speed is: " +speed);
+    }
+
     // TODO: Write a 'brake' method that resets speed to 0
+    void brake() {
+        speed = 0;
+        System.out.println("Brake applies. New speed is: " +speed);
+    }
 }
